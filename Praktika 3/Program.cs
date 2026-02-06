07cf08083205f2879432ceb2fa63ed0c9c9a0a7a
242406 <242406@DESKTOP-NJ0C7QV>
2026-02-06 12:14:09 +0300

    исправление немного

--- Praktika 3/Program.cs
+++ Praktika 3/Program.cs
@@ -10,8 +10,6 @@ namespace dictionary
     {
         static void Main(string[] args)
         {
-            // Практика - Переводчик оценок
-
             // Словарь оценок
             Dictionary<int, string> grades = new Dictionary<int, string>();
             grades.Add(5, "отлично");
@@ -51,39 +49,6 @@ namespace dictionary
                 double average = (double)sum / count;
                 Console.WriteLine($"Средний балл: {average:F1}");
             }
-
-            // Добавление новой оценки
-            Console.WriteLine("Хотите добавить новую оценку? (да/нет):");
-            string answer = Console.ReadLine();
-
-            if (answer == "да")
-            {
-                Console.WriteLine("Введите числовую оценку:");
-                int newNum = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Введите текстовое описание:");
-                string newText = Console.ReadLine();
-
-                grades.Add(newNum, newText);
-                Console.WriteLine($"Оценка {newNum} -> \"{newText}\" добавлена в словарь");
-            }
-
-            // Обратный перевод
-            Console.WriteLine("Введите текстовую оценку:");
-            string search = Console.ReadLine();
-            bool found = false;
-
-            foreach (var grade in grades)
-            {
-                if (grade.Value == search)
-                {
-                    Console.WriteLine($"Числовая оценка: {grade.Key}");
-                    found = true;
-                }
-            }
-
-            if (!found)
-                Console.WriteLine("Такая оценка не найдена");
         }
     }
 }
